ad32cba7ddf8950a7b376199aa68a56d5e96ffcd
Tanishk Dhiman <tanishkdhiman@gmail.com>
2022-07-11 19:45:20 +1200

    Update Scraper.py
    Messed up a bunch of stuff, will take a look later

--- Scraper.py
+++ Scraper.py
@@ -1,6 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = 'http://ethans_fake_twitter_site.surge.sh/?ref=hackernoon.com'
 page = requests.get(URL)
 
-print(page.text)
+#print(page.text)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+tweetcontainer = soup.find("div", class_="tweetcontainer")
+horizDiv = tweetcontainer.find("div", class_="horizontalDivider")
+vertDiv = horizDiv.find("div", class_="verticalDivider")
+author = vertDiv.find("h2", class_="author")
+print(author)
+
+# Name: h2 class="author"
+# Tweet: p class="content"
+# Date: h5 class="dateTime"
